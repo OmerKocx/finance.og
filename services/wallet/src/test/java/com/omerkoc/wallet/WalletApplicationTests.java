feb1335ed022f64182973d5cb2a80045679bca97
Glayment <kocomer670@gmail.com>
2026-07-05 05:39:16 +0300

    wallet service initialization

---
+++ services/wallet/src/test/java/com/omerkoc/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerkoc.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
